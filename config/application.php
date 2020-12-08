a58ed6d1fb517dd7cfc8b0cf6567f2a1b8b2f5c5
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2020-12-08 09:44:41 -0800

    Set Redis prefix via env.

--- config/application.php
+++ config/application.php
@@ -103,9 +103,6 @@ Config::define('AUTH_SALT', env('AUTH_SALT'));
 Config::define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
 Config::define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
 Config::define('NONCE_SALT', env('NONCE_SALT'));
-if(env('WP_CACHE_KEY_SALT')):
-    Config::define('WP_CACHE_KEY_SALT', env('WP_CACHE_KEY_SALT'));
-endif;
 
 /**
  * Custom Settings
